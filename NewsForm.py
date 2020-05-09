7692da9a3121badb875070b99e2a803b017fbd35
Owl-coder <57711937+Owl-coder@users.noreply.github.com>
2020-05-09 08:59:27 +0300

    Add files via upload

---
+++ NewsForm.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+class NewsForm(FlaskForm):
+    title = StringField('Заголовок', validators=[DataRequired()])
+    content = TextAreaField("Содержание")
+    is_private = BooleanField("Личное")
+    submit = SubmitField('Применить')
